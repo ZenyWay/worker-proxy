b4c46863f77b975ef83fdc796037e3d60566f215
S. M. Catala <stephane@zenyway.com>
2016-07-18 13:17:23 +0200

    initial functional implementation that properly runs the README example

---
+++ src/index.ts
@@ -0,0 +1,276 @@
+/// <reference path="../typings/index.d.ts" />
+
+/**
+ * Copyright 2016 Stephane M. Catala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
+;
+import debug = require('debug')
+const log = debug('worker-proxy')
+// import assert = require('assert')
+import Promise = require('bluebird')
+import { newIndexedQueue, IndexedQueue } from './lib/indexed-queue'
+import hookService from './worker'
+
+export { hookService }
+
+/**
+ * @public
+ * @factory ServiceProxyFactory
+ * @param {string} path of `Worker` script
+ * @param {ServiceProxyOpts} opts? {ServiceProxy} configuration options
+ * @generic {S extends Object} service interface
+ * @return {Promise<ServiceProxy<S>>}
+ */
+export interface ServiceProxyFactory {
+  <S extends Object>(path: string, opts?: ServiceProxyOpts): ServiceProxy<S>
+}
+/**
+ * @public
+ * @interface ServiceProxyOpts {ServiceProxy} configuration options
+ */
+export interface ServiceProxyOpts {
+  /**
+   * @public
+   * @prop {number} timeout? for calls to `Worker`
+   */
+  timeout?: number
+}
+/**
+ * @public
+ * @interface
+ * service proxy interface
+ */
+export interface ServiceProxy<S extends Object> {
+  /**
+   * @public
+   * @prop {Promise<S>} service resolves to proxied service object
+   */
+  service: Promise<S>
+  /**
+   * @public
+   * @method terminate call `onterminate` handler in `Worker`
+   * and terminate `Worker` on success,
+   * or abort on error.
+   * @return {Promise<void>} result from `onterminate` handler in `Worker`
+   * @error {Error} from `onterminate` handler in `Worker`
+   */
+  terminate (): Promise<void>
+  /**
+   * @public
+   * @method kill force terminate `Worker`
+   */
+  kill (): void
+}
+
+/**
+ * @public
+ * @interface
+ */
+export interface WorkerServiceEvent extends MessageEvent {
+  data: WorkerServiceEventData
+}
+
+/**
+ * @public
+ * @interface
+ * data content of {WorkerServiceEvent}
+ */
+export interface WorkerServiceEventData extends WorkerServiceMethodCall {
+  uuid: number
+  timeout?: number
+}
+
+export interface WorkerServiceMethodCall {
+  target?: 'service'
+  method: string
+  args?: any[]
+}
+
+/**
+ * @private
+ * @interface Resolver
+ * methods for resolving or rejecting a corresponding {Promise}
+ */
+interface Resolver {
+  resolve: (res?: any|PromiseLike<any>) => void
+  reject: (err?: Error) => void
+}
+
+/**
+ * @private
+ * @class
+ */
+class ServiceProxyClass<S extends Object> implements ServiceProxy<S> {
+	/**
+   * @public
+   * @factory
+	 * @param {string} path of `Worker` script
+   * @generic {S extends Object} service interface
+	 * @return {Promise<ServiceProxy<S>>}
+	 */
+	static newInstance <S extends Object>(path: string,
+  opts?: ServiceProxyOpts): ServiceProxy<S> {
+    if (opts && opts.timeout) { ServiceProxyClass.timeout = opts.timeout }
+
+  	const sp = new ServiceProxyClass(path)
+    log('ServiceProxyClass.newInstance', sp)
+
+  	return <ServiceProxy<S>>({ // revealing module
+      service: sp.service,
+      kill: sp.kill.bind(sp),
+    	terminate: sp.terminate.bind(sp)
+    })
+  }
+  /**
+   * @public
+   * @prop {Promise<S>} service resolves to the proxied service object
+   */
+  service: Promise<S>
+  /**
+   * @public
+   * @method kill force terminate {Worker}
+   */
+  kill (): void {
+    this.worker.terminate()
+  }
+	/**
+   * @public
+   * @method terminate attempt to terminate {Worker}.
+   * abort on error from `onterminate` handler in `Worker`.
+	 * @return {Promise<void>} result from `onterminate` handler in `Worker`
+   * @error {Error} from `onterminate` handler in `Worker`
+	 */
+	terminate (): Promise<void> {
+  	return this.call({ method: 'onterminate' })
+    .then(() => this.kill())
+  }
+  /**
+   * @private
+   * @static
+   * @prop {number} timeout default timeout for calls to `Worker`
+   */
+  static timeout = 3 * 60 * 1000 // ms
+	/**
+   * @private
+   * @constructor
+	 * @param {string} path of `Worker` string
+	 */
+	constructor (path: string) {
+  	this.worker = new Worker(path)
+    this.worker.onmessage = this.onmessage.bind(this)
+    // TODO setup this.worker.onerror ?
+    this.calls = newIndexedQueue<Resolver>()
+    this.service = this.call({  method: 'getServiceMethods' })
+    .then(methods => this.proxy(methods))
+  }
+  /**
+   * @private
+   * @method onmessage `message` event handler.
+   * call method as specified in received `Event.data` {WorkerServiceMethodCall}
+   * @param  {WorkerServiceEvent} event
+   */
+  onmessage (event: WorkerServiceEvent) {
+    const { target, method, args } = event.data
+    const targetMethod = (this[target]||this)[method]||this.unknown
+    log('ServiceProxy.onmessage targetMethod =', targetMethod)
+  	targetMethod.apply(this, args)
+  }
+  /**
+   * @private
+   * @method call place async method call to `Worker`
+   * @param  {WorkerServiceMethodCall} spec of method call
+   * @return {Promise<any>} result from call
+   */
+  call (spec: WorkerServiceMethodCall): Promise<any> {
+    log('ServiceProxy.call spec =', spec)
+    const data = <WorkerServiceEventData>spec
+    return new Promise((resolve, reject) => {
+      data.uuid = this.calls.push({
+        resolve: resolve,
+        reject: reject
+      })
+      this.worker.postMessage(spec)
+    })
+    .timeout(data.timeout || ServiceProxyClass.timeout)
+    .catch(Promise.TimeoutError, err => {
+    	log('ServiceProxy.call uuid =', data.uuid, err)
+      this.calls.pop(data.uuid)
+      return Promise.reject(err)
+    })
+  }
+  /**
+   * @private
+   * @method resolve the {Promise} of a previously placed call,
+   * identified by its `uuid`
+   * @param {number} uuid
+   * @param {any} res
+   */
+  resolve (uuid: number, res: any): void {
+  	log('ServiceProxy.resolve', res)
+    const call = this.calls.pop(uuid)
+    if (call) { call.resolve(res) } // call has not timed out
+  }
+  /**
+   * @private
+   * @method reject the {Promise} of a previously placed call,
+   * identified by its `uuid`
+   * @param {number} uuid
+   * @param {Error} err
+   */
+  reject (uuid: number, err: Error): void {
+  	log('ServiceProxy.reject', err)
+    const call = this.calls.pop(uuid)
+    if (call) { call.reject(err) } // call has not timed out
+  }
+  /**
+   * @private
+   * @method proxy
+   * @param  {string[]} methods
+   * @returns {S} proxied service object from list of methods
+   */
+  proxy (methods: string[]): S {
+  	const sp = this
+  	return methods.reduce((service, method) => {
+      service[method] = function () {
+        return sp.call.call(sp, {
+          target: 'service',
+          method: method,
+          args: Array.prototype.slice.call(arguments) // proper format for postMessage
+        })
+      }
+      return service
+    }, <S>{})
+  }
+  /**
+   * @private
+   * @method unknown
+   * @error {Error} `unknown method`
+   */
+  unknown (): Promise<void> {
+    return Promise.reject(new Error('unknown method'))
+  }
+  /**
+   * @private
+   * @prop {Worker} worker
+   */
+  worker: Worker
+  /**
+   * @private
+   * @prop {IndexedQueue<Resolver>} calls queue of {Resolver} methods
+   * of pending call Promises, indexed by their `uuid`
+   */
+  calls: IndexedQueue<Resolver>
+}
+
+export const newServiceProxy: ServiceProxyFactory =
+ServiceProxyClass.newInstance
---
+++ src/lib/indexed-queue.ts
@@ -0,0 +1,139 @@
+/// <reference path="../../typings/index.d.ts" />
+
+/**
+ * Copyright 2016 Stephane M. Catala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
+;
+import debug = require('debug')
+const log = debug('indexed-queue')
+
+export interface IndexedQueueFactory {
+  <T>(): IndexedQueue<T>
+}
+
+/**
+ * @public
+ * simple indexed queue.
+ *
+ * note: `uuid` indexes start at zero, are generated in unit increments
+ * and wrap to Number.MIN_SAFE_INTEGER when reaching
+ * `!Number.isSafeInteger(uuid)`.
+ * this implementation does not test uuid conflicts.
+ * (very) old entries hence theoretically risk being overridden.
+ * use of this queue hence assumes that entries are only temporarily queued
+ * and removed before the time it takes to wrap all safe integers
+ * (which should practically always be true).
+ * @generic {T} type of queued values
+ */
+export interface IndexedQueue<T> {
+  /**
+   * @public
+   * @method
+   * extract and return the entry indexed by the given `uuid`.
+   * the entry is definitively removed from this `IndexedQueue`.
+   * @param  {number} uuid index
+   * @returns {T} indexed entry
+   */
+  pop (uuid: number): T
+	/**
+   * @public
+   * @method
+   * queue the given `val` and return its `uuid` index
+	 * @param  {T} val
+	 * @returns {number} uuid of queued `val`
+	 */
+	push (val: T): number
+  /**
+   * @public
+   * @method
+   * @returns {number} length of queue
+   */
+  length (): number
+  /**
+   * @public
+   * @method
+   * @param  {number} uuid index
+   * @returns {boolean} true if an entry is queued under the given `uuid`
+   */
+  has (uuid: number): boolean
+}
+
+/**
+ * @public
+ * indexed queue
+ * @generic {T} type of queued values
+ */
+class IndexedQueueClass<T> implements IndexedQueue<T> {
+  static getInstance <T>(): IndexedQueue<T> {
+    return new IndexedQueueClass<T>()
+  }
+  /**
+   * @see {IndexedQueue#pop}
+   */
+  pop (uuid: number): T {
+  	const val = this._queue[uuid]
+    delete this._queue[uuid]
+    this._length--
+    log('Queue.length', this._length)
+    return val
+  }
+	/**
+   * @see {IndexedQueue#push}
+	 */
+	push (val: T): number {
+  	const uuid = this._uuid.next()
+  	this._queue[uuid] = val
+    this._length++
+    log('Queue.length', this._length)
+    return uuid
+  }
+  /**
+   * @see {IndexedQueue#length}
+   */
+  length (): number {
+    return this._length
+  }
+  /**
+   * @see {IndexedQueue#has}
+   */
+  has (uuid: number): boolean {
+  	return uuid in this._queue
+  }
+  /**
+   * @private
+   */
+  _length = 0
+  /**
+   * @private
+   */
+  _uuid = new Uuid()
+  /**
+   * @private
+   */
+  _queue = {}
+}
+
+export const newIndexedQueue: IndexedQueueFactory =
+IndexedQueueClass.getInstance
+
+/**
+ * @private
+ * sequentially incrementing uuid
+ */
+class Uuid {
+  next (): number {
+    return (Number.isSafeInteger(++this.uuid)) ?
+    this.uuid : this.uuid = Number.MIN_SAFE_INTEGER // wrap
+  }
+  uuid = 0
+}
