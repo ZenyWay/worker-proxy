8b5c319b7372150d45c71938fbe92858e99affe9
S. M. Catala <stephane@zenyway.com>
2016-07-25 11:34:15 +0200

    proxy specs: mock all dependencies, initial factory tests

--- spec/proxy/index.spec.ts
+++ spec/proxy/index.spec.ts
@@ -12,7 +12,6 @@
  * Limitations under the License.
  */
 ;
-debugger
 import newServiceProxy, { ServiceProxy } from '../../src/proxy'
 interface Service {
   syncwork: (foo: string, bar: string) => number
@@ -35,29 +34,95 @@ beforeEach(() => {
 
 describe('factory newServiceProxy<S extends Object>(worker: string|Worker, opts?: ' +
 'ServiceProxyOpts): ServiceProxy<S>', () => {
-  describe('when given a {string} path to a worker script', () => {
-    let service: any
-    beforeEach((done) => {
-      const worker = function () {
-        this.onmessage = (event: MessageEvent) => {
-          event.data.method = 'resolve'
-          event.data.args = [ [ 'foo' ] ]
-          this.postMessage(event.data)
-        }
+  let workerFn: () => void
+  let script: string
+  let worker: any
+  let queue: any
+  let getService: (proxy: ServiceProxy<any>, done: DoneFn) => void
+  let service: any // proxied service object
+  let error: Error
+  beforeEach(() => { // mock worker script
+    // resolves calls to predefined values, ignoring target property
+    workerFn = function () {
+      const returnValues = {
+        getServiceMethods: [ 'foo' ],
+        foo: 'foo'
       }
-      const script = `(${worker}())`
-      const blob = new Blob([ script ], { type: 'text/javascript' })
-      const url = URL.createObjectURL(blob) // requires "dom" lib declaration in tsconfig.json
-      const proxy = newServiceProxy(url)
+      this.onmessage = (event: MessageEvent) => {
+        event.data.args = [ returnValues[event.data.method] ]
+        event.data.method = 'resolve'
+        this.postMessage(event.data)
+      }
+    }
+    const blob = new Blob([ `(${workerFn}())` ], { type: 'text/javascript' })
+    script = URL.createObjectURL(blob)
+  })
+  beforeEach(() => { // mock worker from script
+    worker = jasmine.createSpyObj('worker', [ 'postMessage', 'terminate' ])
+    const loopback: any = {
+      postMessage (data: any) {
+        setTimeout(worker.onmessage.bind(worker, { data: data }))
+      }
+    }
+    workerFn.call(loopback)
+    worker.postMessage
+    .and.callFake((data: any) =>
+      setTimeout(loopback.onmessage.bind(loopback, { data: data}))
+    )
+  })
+  beforeEach(() => { // mock queue
+    const _queue: any[] = []
+    queue = jasmine.createSpyObj('queue', [ 'pop', 'push', 'has', 'length'])
+    queue.push.and.callFake(_queue.push.bind(_queue))
+    queue.pop.and.callFake(_queue.pop.bind(_queue))
+    queue.has.and.callFake((index: number) => index <= _queue.length)
+    queue.length.and.callFake(() => _queue.length)
+  })
+  beforeEach(() => {
+    getService = (proxy: ServiceProxy<any>, done: DoneFn): void => {
       proxy.service
       .then(_service => (service = _service))
       .then(setTimeout.bind(undefined, done))
+      .catch((err: Error) => setTimeout(() => {
+        error = err
+        done()
+      }))
+    }
+  })
+  describe('when given a {string} path to a worker script', () => {
+    beforeEach((done) => {
+      const proxy = newServiceProxy(script)
+      getService(proxy, done)
     })
     it('should instantiate and proxy the corresponding worker', () => {
       expect(service).toEqual({ 'foo': jasmine.any(Function) })
     })
-    it('should', () => {
-
+  })
+  describe('when given a {Worker} instance', () => {
+    beforeEach((done) => {
+      const proxy = newServiceProxy(new Worker(script))
+      getService(proxy, done)
+    })
+    it('should instantiate and proxy the corresponding worker', () => {
+      expect(service).toEqual({ 'foo': jasmine.any(Function) })
+    })
+  })
+  describe('when given an {IndexedQueue} instance in "opts"', () => {
+    beforeEach((done) => {
+      const proxy = newServiceProxy(worker, { queue: queue })
+      getService(proxy, done)
+    })
+    it('should use that queue instead of the internal queue', () => {
+      expect(queue.push).toHaveBeenCalled()
+    })
+  })
+  describe('when given a timeout value in "opts"', () => {
+    beforeEach((done) => {
+      const proxy = newServiceProxy(worker, { timeout: 0 /* ms */ })
+      getService(proxy, done)
+    })
+    it('should timeout as specified', () => {
+      expect(error.name).toBe('TimeoutError')
     })
   })
 })
