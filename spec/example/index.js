b3313fdca85f5f2e96b8bbcc74d89a7f0a0725dd
S. M. Catala <stephane@zenyway.com>
2016-07-29 18:21:14 +0200

    update live example and test web page scripts, correct typo in README example

--- spec/example/index.js
+++ spec/example/index.js
@@ -64,6 +64,7 @@ exports.default = newIndexGenerator;
 var indexed_queue_1 = require('./indexed-queue');
 var utils_1 = require('../common/utils');
 var Promise = require('bluebird');
+var tslib_1 = require('tslib');
 var debug = require('debug');
 var log = debug('worker-proxy');
 var ServiceProxyClass = (function () {
@@ -113,7 +114,7 @@ var ServiceProxyClass = (function () {
     ServiceProxyClass.prototype.call = function (spec) {
         var _this = this;
         log('ServiceProxy.call', spec);
-        var data = Object.assign({}, spec);
+        var data = tslib_1.__assign({}, spec);
         var result = new Promise(function (resolve, reject) {
             data.uuid = _this.calls.push({
                 resolve: resolve,
@@ -159,7 +160,7 @@ var newServiceProxy = ServiceProxyClass.newInstance;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = newServiceProxy;
 
-},{"../common/utils":1,"./indexed-queue":4,"bluebird":5,"debug":6}],4:[function(require,module,exports){
+},{"../common/utils":1,"./indexed-queue":4,"bluebird":5,"debug":6,"tslib":10}],4:[function(require,module,exports){
 "use strict";
 ;
 var index_generator_1 = require('./index-generator');
@@ -6308,6 +6309,98 @@ process.chdir = function (dir) {
 process.umask = function() { return 0; };
 
 },{}],10:[function(require,module,exports){
+(function (global){
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global global, define, System, Reflect, Promise */
+var __extends;
+var __assign;
+var __decorate;
+var __param;
+var __metadata;
+var __awaiter;
+(function (factory) {
+    var root = typeof global === "object" ? global : typeof self === "object" ? self : typeof this === "object" ? this : {};
+    if (typeof System === "object" && typeof System.register === "function") {
+        System.register("tslib", [], function (exporter) {
+            factory(createExporter(root, exporter));
+            return { setters: [], execute: function() { } };
+        });
+    }
+    else if (typeof define === "function" && define.amd) {
+        define("tslib", ["exports"], function (exports) { factory(createExporter(root, createExporter(exports))); });
+    }
+    else if (typeof module === "object" && typeof module.exports === "object") {
+        factory(createExporter(root, createExporter(module.exports)));
+    }
+    else {
+        factory(createExporter(root));
+    }
+
+    function createExporter(exports, previous) {
+        return function (id, v) { return exports[id] = previous ? previous(id, v) : v; };
+    }
+})
+(function (exporter) {
+    __extends = function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+
+    __assign = Object.assign || function (t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+
+    __decorate = function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+
+    __param = function (paramIndex, decorator) {
+        return function (target, key) { decorator(target, key, paramIndex); }
+    };
+
+    __metadata = function (metadataKey, metadataValue) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(metadataKey, metadataValue);
+    };
+
+    __awaiter = function (thisArg, _arguments, P, generator) {
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+            function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+            function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+            step((generator = generator.apply(thisArg, _arguments)).next());
+        });
+    };
+
+    exporter("__extends", __extends);
+    exporter("__assign", __assign);
+    exporter("__decorate", __decorate);
+    exporter("__param", __param);
+    exporter("__metadata", __metadata);
+    exporter("__awaiter", __awaiter);
+});
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],11:[function(require,module,exports){
 "use strict";
 ;
 var proxy_1 = require('../../dist/proxy');
@@ -6323,4 +6416,4 @@ proxy.service
     .then(terminate)
     .catch(function (err) { return log(err) || proxy.kill(); });
 
-},{"../../dist/proxy":3,"debug":6}]},{},[10]);
+},{"../../dist/proxy":3,"debug":6}]},{},[11]);
